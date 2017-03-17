058e963e66bd4b6eb228db7fa8f5fe865c55e62c
vchernogorov <vchernogorov@griddynamics.com>
2017-03-17 23:02:16 +0400

    Add  searchbox

--- ui/app/app.js
+++ ui/app/app.js
@@ -3,11 +3,14 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngAnimate',
   'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'paasb'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+config(['$locationProvider', '$routeProvider',
+  function($locationProvider, $routeProvider) {
+
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
