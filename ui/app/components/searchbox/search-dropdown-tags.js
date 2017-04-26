b3117899b766738ff1ae9896339348bb37e27b55
vchernogorov <vchernogorov@griddynamics.com>
2017-04-26 14:25:53 +0400

    Create add-content directory and rempve search-dropdown-tags.js

--- ui/app/components/searchbox/search-dropdown-tags.js
+++
@@ -1,9 +0,0 @@
-$(documnent).ready(function() {
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutationRecord) {
-      mutationRecord.appendChild()
-    });
-  });
-  var target = $('.tags-suggestion-list');
-  observer.observe(target, { attributes : true, attributeFilter : ['style'] });
-});
