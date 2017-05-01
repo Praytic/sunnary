70b92b8a940870b10f6ab17316a5bda33d74a499
vchernogorov <vchernogorov@griddynamics.com>
2017-05-01 13:40:34 +0400

    Move configurations and factories to main module

--- ui/app/app.js
+++ ui/app/app.js
@@ -4,35 +4,4 @@ angular.module('myApp', [
       'ngRoute',
       'ngAnimate',
       'myApp.main'
-    ])
-
-    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
-      $routeProvider
-          .when('/search', {
-            template: "<div search-results></div>"
-          })
-          .when("/add", {
-            template: "<div id='add-content' add-content-card></div>"
-          })
-          .otherwise({redirectTo: '/'});
-    }])
-
-    .factory('Tag', ['$http', function($http) {
-      return {
-        get: function() {
-          return $http.get('http://localhost:8888/api/tags');
-        },
-        getByQuery: function(query) {
-          return $http.get('http://localhost:8888/api/tags?q=' + query);
-        }
-      }
-    }])
-
-    .factory('Content', ['$http', function($http) {
-      return {
-        search: function(tags) {
-          return $http.post('http://localhost:8888/api/content/search', tags);
-        }
-      }
-    }]);
+    ]);
--- ui/app/main/main.js
+++ ui/app/main/main.js
@@ -6,4 +6,42 @@ angular.module('myApp.main', [
   'ui.bootstrap',
   'myApp.mySearchbox',
   'myApp.searchResults',
-  'myApp.addContentCard']);
+  'myApp.addContentCard'])
+
+    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+      $routeProvider
+          .when('/search', {
+            template: "<div search-results></div>"
+          })
+          .when("/add", {
+            template: "<div id='add-content' add-content-card></div>"
+          })
+          .otherwise({redirectTo: '/'});
+    }])
+
+    .directive('mainPage', function () {
+      return {
+        restrict: 'A',
+        templateUrl: 'main/main-page.html'
+      };
+    })
+
+    .factory('Tag', ['$http', function($http) {
+      return {
+        get: function() {
+          return $http.get('http://localhost:8888/api/tags');
+        },
+        getByQuery: function(query) {
+          return $http.get('http://localhost:8888/api/tags?q=' + query);
+        }
+      }
+    }])
+
+    .factory('Content', ['$http', function($http) {
+      return {
+        search: function(tags) {
+          return $http.post('http://localhost:8888/api/content/search', tags);
+        }
+      }
+    }]);
