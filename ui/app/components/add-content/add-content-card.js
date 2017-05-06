4a3e2dff59f80d4317100e1e5fd005c2095dc916
vchernogorov <vchernogorov@griddynamics.com>
2017-05-06 20:16:24 +0400

    Create template for AddContent page

--- ui/app/components/add-content/add-content-card.js
+++ ui/app/components/add-content/add-content-card.js
@@ -5,6 +5,15 @@ angular.module('myApp.addContentCard', [])
     .directive('addContentCard', function () {
       return {
         restrict: 'A',
-        templateUrl: 'components/add-content/add-content-card.html'
+        templateUrl: 'components/add-content/add-content-card.html',
+        controller: ['$scope', '$location', '$http', 'Content',
+          function AddContentCtrl($scope, $location, $http, Content) {
+            $scope.type = Object.freeze({
+              TOOL: 0,
+              ARTICLE: 1,
+              PACK: 2
+            });
+            $scope.currentType = $scope.type.TOOL;
+          }]
       };
     });
