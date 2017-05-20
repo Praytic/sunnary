2a31ab4d1007060a0839e5d12dfa4d79b37a4e19
vchernogorov <vchernogorov@griddynamics.com>
2017-05-20 13:55:50 +0400

    Create google oauth2

--- server/src/main/java/net/sunnary/sunnary/Application.java
+++ server/src/main/java/net/sunnary/sunnary/Application.java
@@ -2,9 +2,11 @@ package net.sunnary.sunnary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- server/src/main/java/net/sunnary/sunnary/SunnaryConfiguration.java
+++ server/src/main/java/net/sunnary/sunnary/SunnaryConfiguration.java
@@ -1,5 +1,6 @@
 package net.sunnary.sunnary;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableOAuth2Sso
 public class SunnaryConfiguration {
 
     @Bean
